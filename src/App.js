640dc509f66f2c73a93be4a7d11721fd16cb33fc
carmenllenado <39427352+carmenllenado@users.noreply.github.com>
2023-11-28 19:19:28 +0800

    Added GitHub repo url

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React from "react";
+
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
        Hello World
+       <footer>
+        This project was coded by Carmen Llenado and is {" "}
+        <a href="https://github.com/carmenllenado/weather-app-react"
+        target="_blank">open-sourced on GitHub</a>
+       </footer>
     </div>
   );
 }
