ea35a05cf61739081febfe73dbe7d48bfe32a2c6
carmenllenado <39427352+carmenllenado@users.noreply.github.com>
2023-11-29 16:52:55 +0800

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
        Hello World
        <footer>
         This project was coded by Carmen Llenado and is {" "}
         <a href="https://github.com/carmenllenado/weather-app-react"
         target="_blank" rel="noreferrer">open-sourced on GitHub</a>
        </footer>
+       </div>
     </div>
   );
 }
