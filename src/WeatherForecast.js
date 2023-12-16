e828826cf139def8f07990a617d72bcee4621c1a
carmenllenado <39427352+carmenllenado@users.noreply.github.com>
2023-12-16 18:47:58 +0800

    Created daily weather forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -6,6 +6,13 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
     let [loaded, setLoaded] = useState(false);
     let [forecast, setForecast] = useState(null);
+
+    useEffect(() => {
+        setLoaded(false);
+    }, [props.coordinates]);
+
+    // if the coordinates change
+    // set loaded false
 
     function handleResponse(response) {
         setForecast(response.data.daily);
